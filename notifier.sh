98c0fbea7612301bcbc42250fbd2bd34d9273991
Qiyas <107604130+qiyascc@users.noreply.github.com>
2023-09-11 21:51:25 +0400

    Edit notifier.sh

--- notifier.sh
+++ notifier.sh
@@ -6,6 +6,6 @@ IP_EMOJI=$'\U1F4EE'
 USER_EMOJI=$'\U1F464'
 
 if [ ${PAM_TYPE} = "open_session" ]; then
-  MESSAGE=$(printf "${TARGET_EMOJI} New Login detected!\n\n${IP_EMOJI} IP: %s\n>
-  curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendMessage" -d >
+  MESSAGE=$(printf "**${TARGET_EMOJI} New Login detected!**\n\n${IP_EMOJI} **IP:** %s\n${USER_EMOJI} **User:** %s\n⌨ **Login Type:** %s" "$PAM_RHOST" "$PAM_USER" "$PAM_SERVICE")
+  curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendMessage" -d "chat_id=$CHAT_ID&text=$MESSAGE&parse_mode=Markdown"
 fi
